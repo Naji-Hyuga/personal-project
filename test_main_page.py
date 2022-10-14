da5d8535aee3ed440fae10f102554f348c0e1d88
Khasanov Adel <a.khasanov@id-network.ru>
2022-10-14 21:46:09 +0300

    moved the locators to a separate file - "locators"

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,19 @@
+# -*- coding: utf-8 -*-
+from pages.main_page import MainPage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 link = "http://selenium1py.pythonanywhere.com/"
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
 
 def test_guest_can_go_to_login_page(browser):
     browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
+
+#Метод проверяющий наличие ссылки
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
